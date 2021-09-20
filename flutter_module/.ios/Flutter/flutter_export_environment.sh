989b0e18992b1687cf7c5f37e8a18bad5c01eae4
DuQian <duqian2010@gmail.com>
2021-09-20 10:42:30 +0800

    增加flutter for ios

--- flutter_module/.ios/Flutter/flutter_export_environment.sh
+++ flutter_module/.ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\AndroidDev\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\DusanAndroid\Android-Lab-DQ\flutter_module"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/duqian/AndroidDev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/duqian/Development/MyGitHub/DQ-Android-Labs/flutter_module"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
